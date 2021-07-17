fa46ec728083caa7c93646be6f39d512c3bbbc2f
RTBlanco <ronnytoribio1@hotmail.com>
2021-07-17 05:38:27 -0400

    made it so foods will have access to images

--- virtual-menu-backend/app/models/food.rb
+++ virtual-menu-backend/app/models/food.rb
@@ -1,3 +1,11 @@
 class Food < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :category
+
+  has_one_attached :image
+  
+  def get_image_url
+    url_for(self.image)
+  end
 end
