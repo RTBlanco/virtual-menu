001f208426030adae81e359c9b94e8e29d675ba4
RTBlanco <ronnytoribio1@hotmail.com>
2021-07-17 19:21:02 -0400

    imported correct librays

--- virtual-menu-frontend/src/containers/App.js
+++ virtual-menu-frontend/src/containers/App.js
@@ -2,6 +2,7 @@ import Name from "../components/Name";
 import NavBar from "../components/NavBar";
 import About from "../components/About";
 import Categories from "./Categories";
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
