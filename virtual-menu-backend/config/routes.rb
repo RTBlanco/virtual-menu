06b719002151cc1d3c10e578a528a0847d4c5a8f
RTBlanco <ronnytoribio1@hotmail.com>
2021-07-17 04:47:18 -0400

    changed routes to reflect association

--- virtual-menu-backend/config/routes.rb
+++ virtual-menu-backend/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :admins
-  resources :foods
-  resources :categories
-  resources :resturants
+  
+  resources :resturants do
+    resource :admins 
+    resources :categories do 
+      resources :foods
+    end 
+  end
+    
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
