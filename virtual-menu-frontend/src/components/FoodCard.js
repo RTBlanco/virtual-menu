1cc3c0dd11dcb1060b3765185bb8efb5cb122e41
RTBlanco <ronnytoribio1@hotmail.com>
2021-07-31 04:38:47 -0400

    seperated card and food-card

--- virtual-menu-frontend/src/components/FoodCard.js
+++ virtual-menu-frontend/src/components/FoodCard.js
@@ -2,7 +2,7 @@
 const FoodCard = ({food}) => {
 
   return (
-    <div className="card">
+    <div className="food-card">
       <h1>{food.name}</h1>
       <img src={food.image} alt="food" />
     </div>
